34069ee5b70bf4a0c779622698dc1dcb12d8e362
Alex Beltyukov <xonika9@gmail.com>
2022-07-19 14:40:47 +0800

    make new function that toggles like

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -46,15 +46,9 @@ class Api {
       headers: this._headers,
     }).then(this._checkResponse);
   };
-  addLike = (cardId) => {
-    return fetch(`${this._baseUrl}cards/${cardId}/likes`, {
-      method: 'PUT',
-      headers: this._headers,
-    }).then(this._checkResponse);
-  };
-  removeLike = (cardId) => {
-    return fetch(`${this._baseUrl}cards/${cardId}/likes`, {
-      method: 'DELETE',
+  toggleLike = (cardId, isLiked) => {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: `${isLiked ? 'DELETE' : 'PUT'}`,
       headers: this._headers,
     }).then(this._checkResponse);
   };
