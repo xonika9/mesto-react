e8afd785058550b7d0291188fdca091dd9dd321d
Alex Beltyukov <xonika9@gmail.com>
2022-07-19 15:23:56 +0800

    add props, fix classNames and message

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,6 +1,20 @@
-function PopupWithForm({ name, title, acceptMessage, children, isOpened, onClose }) {
+function PopupWithForm({
+  name,
+  title,
+  acceptMessage,
+  children,
+  isOpened,
+  onClose,
+  onSubmit,
+  isLoading,
+  isValid,
+}) {
   return (
-    <div className={`popup popup_type_${name}${isOpened ? ' popup_is-opened' : ''}`}>
+    <div
+      className={`popup popup_type_${name}${
+        isOpened ? ' popup_is-opened' : ''
+      }`}
+    >
       <div className='popup__content'>
         <button
           className='popup__close-button'
@@ -9,10 +23,22 @@ function PopupWithForm({ name, title, acceptMessage, children, isOpened, onClose
           onClick={onClose}
         ></button>
         <h2 className='popup__title'>{title}</h2>
-        <form className='popup__form' name={`${name}-form`} noValidate>
+        <form
+          className='popup__form'
+          name={`${name}-form`}
+          noValidate
+          onSubmit={onSubmit}
+        >
           {children}
-          <button className='popup__submit-button' type='submit' aria-label={acceptMessage}>
-            {acceptMessage}
+          <button
+            className={`popup__submit-button ${
+              !isValid && 'popup__submit-button_disabled'
+            }`}
+            type='submit'
+            disabled={!isValid}
+            aria-label={acceptMessage}
+          >
+            {isLoading ? 'Сохранение...' : acceptMessage}
           </button>
         </form>
       </div>
